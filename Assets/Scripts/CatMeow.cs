4b8bb6b2dbec983e71b17b7bb7b1b505eb4dd30f
Andreea Bîncă <andreea.binca.17@gmail.com>
2023-06-17 23:49:57 +0300

    Add audio

---
+++ Assets/Scripts/CatMeow.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatMeow : MonoBehaviour
+{
+    public AudioClip meowOne;
+    public AudioClip meowTwo;
+
+    private float frequency;
+    private float timePassed;
+
+    void Start()
+    {
+        AudioSource.PlayClipAtPoint(meowOne, transform.position, 1f);
+        frequency = Random.Range(100, 500);
+        timePassed = 0;
+    }
+
+    void Update()
+    {
+        timePassed += Time.deltaTime;
+
+        if (timePassed > frequency)
+        {
+            if (frequency % 2 == 0)
+                AudioSource.PlayClipAtPoint(meowOne, transform.position, 1f);
+            else
+                AudioSource.PlayClipAtPoint(meowTwo, transform.position, 1f);
+
+            frequency = Random.Range(100, 500);
+            timePassed = 0;
+        }
+    }
+}
